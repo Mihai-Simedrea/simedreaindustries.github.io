3cca302421289c415061b9f09dfb57c4437f593a
Mihai-Simedrea <mihai.nicolae.simedrea@gmail.com>
2026-04-15 16:11:34 +0300

    fix: base url

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/simedreaindustries.github.io/',
+  base: '/',
   plugins: [
     tailwindcss(),
     react(),
